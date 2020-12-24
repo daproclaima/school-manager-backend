0c54fdbd91e1417249a6b4a4a85098b082bbb893
daproclaima <sn.itpro@gmail.com>
2020-12-24 17:53:55 +0100

    initial commit use graphQL and mongoDB add entity, resolver and service, graph type for lesson configures modules with right imports and injections

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,29 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(
+    private lessonService: LessonService
+  ) {
+  }
+  @Query((returns) => LessonType)
+  lesson() {
+    return {
+      id: 'adad',
+      name: 'afaf',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }
+}
