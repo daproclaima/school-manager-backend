3dd52db7435811b7a397b0ca4d146144424d4edf
daproclaima <sn.itpro@gmail.com>
2020-12-24 19:36:25 +0100

    feat(lesson, package): add validator and creates dto for lesson

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,26 +1,21 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
-  constructor(
-    private lessonService: LessonService
-  ) {
-  }
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
-  lesson(
-    @Args('id') id: string,
-  ) {
+  lesson(@Args('id') id: string) {
     return this.lessonService.getLesson(id);
   }
 
   @Mutation((returns) => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
-    return this.lessonService.createLesson(name, startDate, endDate);
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
